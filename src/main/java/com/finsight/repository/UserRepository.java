fae30f616b1121a236a059c3b4acd4c3d6ee643b
HumphreyMahlangu <godasamukelo757@gmai.com>
2026-05-15 01:51:47 +0200

    feat: add JWT service, security filter and application config

---
+++ src/main/java/com/finsight/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.finsight.repository;
+
+import com.finsight.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
